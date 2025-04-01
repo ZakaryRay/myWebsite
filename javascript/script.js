c1bab5e814439973dc2c371429fc487344d4a378
ZakaryRay <zakary-raymond@live.fr>
2025-03-31 21:58:22 -0400

    JS - Update checkbox function

--- javascript/script.js
+++ javascript/script.js
@@ -1,19 +1,34 @@
 const throwDiceBtn = document.getElementById("throwDiceBtn");
-const diceValue = document.getElementsByClassName("diceValue");
 
 throwDiceBtn.addEventListener("click", () => {
     displayDices();
+    verifyIfCheckBoxAreChecked();
 });
 
-const displayDices = () => {
-    let diceValue = document.querySelectorAll(".dice .diceValue");
+const generateRandomNumberOneToSix = () => {
+    let number = Math.floor(Math.random() * 6) + 1;
+    return number;
+};
+let diceValue = document.querySelectorAll(".dice .diceValue");
 
+const displayDices = () => {
     diceValue.forEach((dice, index) => {
         dice.innerText = generateRandomNumberOneToSix();
     });
 };
 
-const generateRandomNumberOneToSix = () => {
-    let number = Math.floor(Math.random() * 6) + 1;
-    return number;
+const checkBox = document.querySelectorAll(".dice input");
+
+checkBox.forEach((box, index) => {
+    box.addEventListener("change", () => {
+        if (box.checked) {
+            console.log(`Box number ${index + 1} is checked`);
+        } else {
+            console.log(`Box number ${index + 1} is not checked`);
+        }
+    });
+});
+
+const verifyIfCheckBoxAreChecked = () => {
+    checkBox.forEach((box) => box.dispatchEvent(new Event("change")));
 };
